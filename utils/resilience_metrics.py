007b966c51b94c766d85e75417f4794d452d8bd1
NoFishLikeIan <andreatitton96@gmail.com>
2020-06-22 20:02:29 +0200

    🐛 fixed issue with zero index stability

--- utils/resilience_metrics.py
+++ utils/resilience_metrics.py
@@ -3,7 +3,7 @@ from typing import Tuple
 import pandas as pd
 import numpy as np
 
-def resilience(sim: pd.DataFrame, idxs = []) -> Tuple[float, float]:
+def resilience(sim: pd.DataFrame, rec_th = 1., idxs = []) -> Tuple[float, float]:
     """
     Parameters
     ----------
@@ -16,8 +16,8 @@ def resilience(sim: pd.DataFrame, idxs = []) -> Tuple[float, float]:
         shock intensity, time to recovery
     """        
     
-    average_shock = sim.min()
-    time_to_rec = (sim >= 1.).idxmax()
+    average_shock = sim.min() - 1
+    time_to_rec = (sim >= rec_th)[1:].idxmax()
 
     if len(idxs) > 0:
         return average_shock[idxs], time_to_rec[idxs]
