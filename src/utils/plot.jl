a3ecada11b69c0532d17b0d112f799297acd7831
NoFishLikeIan <andreatitton96@gmail.com>
2020-12-01 18:37:52 +0100

    👨‍🎨 simple presentation plots

--- src/utils/plot.jl
+++ src/utils/plot.jl
@@ -6,9 +6,11 @@ function mapto01(x::Float64, a::Float64, b::Float64)::Float64
     (x - a) / (b - a)
 end
 
-function plotgraph(g, path::String, title::String)
+function plotgraph(
+    g, path::String, title::String;
+    coherence::IncoherenceLevels=nothing)
 
-    F0, h = computeincoherence(g)
+    F0, h = isnothing(coherence) ? computeincoherence(g) : coherence
 
     colors = get(ColorSchemes.redblue, mapto01.(h, -1., 1.))
     edge_colors = get(ColorSchemes.dense, [e.weight for e in edges(g)])
