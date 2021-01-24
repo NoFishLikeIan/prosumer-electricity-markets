4a1a6984013a51cb6a310880af8593ce4a0399d9
NoFishLikeIan <andreatitton96@gmail.com>
2021-01-24 14:55:03 +0100

    🥕 endogenous grid method with root solver, not stable

--- src/algos/endgrid.jl
+++ src/algos/endgrid.jl
@@ -1,19 +1,23 @@
+"""
+Compute a policy using the endogenous grid method, 
+assuming exogenous forecasting rule Ψ
+"""
 function endgrid(
-    gridsizes::Tuple{2,Int}, prosumer::Prosumer, environment::Environment;
-    maxiter=1_000, tol=1e-3, verbose=false, gridbounds=[0., 10.]
+    gridsizes::Tuple{Int,Int}, prosumer::Prosumer, environment::Environment;
+    maxiter=1_000, tol=1e-3, verbose=false, gridbounds=[0., 10.], ρ0=1.
 )
     @unpack weather = environment
-    @unpack u, u′, ψ₁, ψ₂ = prosumer
-    invu′ = u′
+    @unpack u, u′, ψ₁, ψ₂, β = prosumer
+    invu′ = u′ # FIXME: Only works if u′ = invu′, latter should come from prosumer
 
     endowments = weather.state_values
     D_e = length(endowments)
     N_c, N_p = gridsizes
 
     c_grid = range(gridbounds..., length=N_c)
-    p_grid = range(gridbounds..., length=N_p)
+    p_grid = range(0.01, 1.0, length=N_p) # TODO: Bounds for p?
 
-    policy = repeat(c_grid, 1, N_p, D_e)
+    policy = ones(N_c, N_p, D_e)
     space = collect.(Iterators.product(c_grid, p_grid, endowments)) 
 
     spaceidx = cartesianfromsize(N_c, N_p, D_e)
@@ -23,16 +27,33 @@ function endgrid(
         g = positive ∘ fromMtoFn(policy, c_grid, p_grid, endowments)
         inverse = similar(policy)
 
+        x(c, p, e) = e + (c - g(c, p, e)) / p # Energy demand function
+
         @threads for (i, j, k) in spaceidx
+    
             # Endogenous grid method for each c′
             c′, p = c_grid[i], p_grid[j]
             ϵ = endowments[k]
 
-            p′ = ψ₁ * p
+            p′ = ψ₁ * p # Forecast of price
+            P = weather.p[k, :]
 
-            c = 0. # TODO: Use Euler to find "c"
+            x′ = x.(c′, p′, endowments) # Next period demand, state contingent 
 
-            inverse[i, j, k] = c
+            rhs = β * (P'u′.(x′))
+            Δc = (invu′(rhs) - ϵ) * p # c - g(c, p, e)
+            f(x) = x - g(x, p, ϵ) - Δc # Root function of Δc
+            
+            try
+                c = find_zero(f, g(0., p, ϵ)) # Use previous c as guess
+                # print("c = $c\n")
+                inverse[i, j, k] = c
+
+            catch e
+                print("(i, j, k) = ($i, $j, $k)\n")
+                throw(e)
+            end
+
         end
         
         newpolicy = similar(policy)
@@ -47,18 +68,18 @@ function endgrid(
 
         # Updating
         d = newpolicy - policy
-        err_distance = maximum(abs.(d))
-        verbose && print("Iteration $iter / $max_iter: $(@sprintf("%.4f", err_distance)) \r")
+        errdistance = maximum(abs.(d))
+        verbose && print("Iteration $iter / $maxiter: $(@sprintf("%.4f", errdistance)) \r")
 
-        if err_distance < tol 
-            verbose && print("Found policy in $iter iterations (|x - x'| = $(@sprintf("%.4f", err_distance)))\n")
+        if errdistance < tol 
+            verbose && print("Found policy in $iter iterations (|x - x'| = $(@sprintf("%.4f", errdistance)))\n")
             return g
         end
 
-        ρ = ρ0 - iter / max_iter # Dynamic dumping parameter
+        ρ = ρ0 - iter / maxiter # Dynamic dumping parameter
         policy += ρ * d # Update with dumping parameter
 
     end
     
-    throw(ConvergenceException(max_iter))
+    throw("Did not converge in $maxiter iterations")
 end
