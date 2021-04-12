20b5677037e1309ebb99afd89149ee40b98ade0c
NoFishLikeIan <andreatitton96@gmail.com>
2021-04-12 09:51:32 +0200

    🧹 setup main for grid market

--- src/main.jl
+++ src/main.jl
@@ -5,16 +5,17 @@ using FiniteDifferences
 using Random, Distributions
 
 using Plots, Logging, Printf
+using LightGraphs, GraphPlot
 using StatsPlots, DataFrames
 
 Random.seed!(11148705)
-Plots.scalefontsizes(.6)
 
 include("utils.jl")
-include("markets/local.jl")
-include("markets/oneglobal.jl")
 
+include("markets/local.jl")
 include("algos/endgrid.jl")
+
+include("markets/global.jl")
 
 include("simulation/simutils.jl")
 include("simulation/decision.jl")
@@ -23,36 +24,29 @@ include("simulation/simulation.jl")
 include("plotting/local.jl")
 include("plotting/global.jl")
 
-prosumer = Prosumer(ψ₁=.95, ψ₂=1.01)
-environment = Environment(ω=0.5, γ=0.5)
-
-
-sizes = (600, 100)
-pess, opt = solvepolicy(sizes, prosumer, environment; verbose=true, tol=1e-2)
-
-policy_plots = true
-
-if policy_plots
-    plotrules(pess, opt, environment, prosumer)
-    plotg(pess, environment, prosumer)
-    plotdemand(pess, environment)
-    
-    plotsimplesolution(pess, opt, environment; m=10.)
-end
-
-N, T = 20, 100
-ms, xs, policy, es, p = simulate(
-    pess, opt, prosumer, environment, 
-    T, N; 
-    verbose=true, exog=true)
 
 simulation_plots = false
 
 if simulation_plots
+
+    Plots.scalefontsizes(.6)
+
+    prosumer = Prosumer(ψ₁=.95, ψ₂=1.01)
+    environment = Environment(ω=0.5, γ=0.5)
+
+    sizes = (600, 100)
+    pess, opt = solvepolicy(sizes, prosumer, environment; verbose=true, tol=1e-2)
+
+
+    N, T = 20, 100
+    ms, xs, policy, es, p = simulate(
+    pess, opt, prosumer, environment, 
+    T, N; 
+    verbose=true, exog=true)
+
     plotsimulation(xs, es; policy=policy)
     plottypes(policy, es)
     plotpricesdemand(xs, es, p)
+
+    Plots.resetfontsizes()
 end
-
-
-Plots.resetfontsizes()
