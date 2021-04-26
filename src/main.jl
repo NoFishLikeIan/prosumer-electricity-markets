50ae1c9c3684ebced1fed30b0854f7950e85c5d1
NoFishLikeIan <andreatitton96@gmail.com>
2021-04-26 16:31:45 +0200

    🧹 cleaned junk code

--- src/main.jl
+++ src/main.jl
@@ -1,8 +1,9 @@
 using Parameters, QuantEcon
 using Base.Threads
-using Interpolations, Roots
+using Interpolations, Roots, NLsolve
 using FiniteDifferences
 using Random, Distributions
+using LinearAlgebra
 
 using Plots, Logging, Printf
 using LightGraphs, GraphPlot
@@ -16,6 +17,7 @@ include("markets/local.jl")
 include("algos/endgrid.jl")
 
 include("markets/global.jl")
+include("markets/gridfirm.jl")
 
 include("simulation/simutils.jl")
 include("simulation/decision.jl")
@@ -24,30 +26,3 @@ include("simulation/demand.jl")
 
 include("plotting/local.jl")
 include("plotting/global.jl")
-
-
-simulation_plots = false
-
-if simulation_plots
-
-    Plots.scalefontsizes(.6)
-
-    prosumer = Prosumer(ψ₁=.95, ψ₂=1.01)
-    environment = Environment(ω=0.5, γ=0.5)
-
-    sizes = (600, 100)
-    pess, opt = solvepolicy(sizes, prosumer, environment; verbose=true, tol=1e-2)
-
-
-    N, T = 20, 100
-    ms, xs, policy, es, p = simulate(
-    pess, opt, prosumer, environment, 
-    T, N; 
-    verbose=true, exog=true)
-
-    plotsimulation(xs, es; policy=policy)
-    plottypes(policy, es)
-    plotpricesdemand(xs, es, p)
-
-    Plots.resetfontsizes()
-end
