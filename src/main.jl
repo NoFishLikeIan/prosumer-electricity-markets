fb80ffec148ada5ce0c630de0179b1f024191455
NoFishLikeIan <andreatitton96@gmail.com>
2021-05-26 15:32:07 +0200

    🧹 clearup of previous model

--- src/main.jl
+++ src/main.jl
@@ -1,6 +1,5 @@
 using Parameters, QuantEcon
 using Base.Threads
-using Interpolations, Roots, NLsolve
 using FiniteDifferences
 using Random, Distributions
 using LinearAlgebra
@@ -14,15 +13,10 @@ Random.seed!(11148705)
 include("utils.jl")
 
 include("markets/local.jl")
-include("algos/endgrid.jl")
 
 include("markets/global.jl")
 include("markets/gridfirm.jl")
 
 include("simulation/simutils.jl")
-include("simulation/decision.jl")
-include("simulation/simulation.jl")
-include("simulation/demand.jl")
 
-include("plotting/local.jl")
 include("plotting/global.jl")
