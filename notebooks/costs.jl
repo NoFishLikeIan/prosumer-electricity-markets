07d8cf5521b7ca5e19e308a2f38d4291bad4ff4f
NoFishLikeIan <andreatitton96@gmail.com>
2021-06-30 11:17:48 +0200

    🪐 provider section simulation

--- notebooks/costs.jl
+++ notebooks/costs.jl
@@ -13,8 +13,17 @@ end
 
 # ╔═╡ 4509a0c9-374e-4f0f-a5dd-5671f0c67bb4
 begin
+	Plots.reset_defaults()
 	Plots.resetfontsizes()
-	Plots.scalefontsizes(0.8)
+end
+
+# ╔═╡ e2ac63a4-6440-41d6-9e05-6e31c6f366cb
+begin
+	upscale = 3 #8x upscaling in resolution
+	fntsm = Plots.font("sans-serif", pointsize=round(10.0*upscale))
+	fntlg = Plots.font("sans-serif", pointsize=round(14.0*upscale))
+	default(titlefont=fntlg, guidefont=fntlg, tickfont=fntsm, legendfont=fntsm)
+	default(size=(800*upscale,600*upscale)) #Plot canvas size
 end
 
 # ╔═╡ 805018be-7695-45ae-a671-2f8d8cc4a858
@@ -27,6 +36,7 @@ begin
 	p = 5.0
 	β = 0.5
 	k = 2.0
+	γ = 0.5
 end
 
 # ╔═╡ e572d85f-1839-48e5-b1e2-2ee1123ef22e
@@ -79,7 +89,7 @@ function plotfoc(c, ∂c∂s, ∂c∂r; rs = range(-s, s, length = 500), s = s)
 	
 	figure = plot(
 		costfig, derivfig, marginalfig, 
-		layout = l, figsize = (1200, 800), dpi = 500)
+		layout = l)
 	
 	return figure
 	
@@ -119,39 +129,90 @@ figuresoft = plotfoc(c, ∂c∂s, ∂c∂r)
 # ╔═╡ 93e56234-86d8-423b-8325-42d0ab2fa3de
 # savefig(figuresoft, joinpath(plotpath, "cost.pdf"))
 
+# ╔═╡ b76606b0-3e1f-4d7d-bb88-b39e41827a67
+function r(s, p, ψ)
+	unitπ = ψ*p - k
+	
+	if unitπ < 0 return -γ*s end
+	
+		
+	βf = (1 - β)/β
+	invβf = inv(βf)
+
+	if s > invβf * √(unitπ)
+		return βf * s - 0.5 * √((2βf * s)^2 - 4unitπ)
+	else
+		return invβf * √(unitπ) - s
+	end
+
+		
+end
+
+# ╔═╡ 52bc7382-42c8-4b4c-b188-3c8c9af281bf
+begin
+	sᵤ = 10.0
+	supplyspace = range(0.0, sᵤ, length = 1000)
+	pricespace = range(0.0, 100.0, length = 1000)
+end
+
+# ╔═╡ 9dbece0b-2aaf-475e-b45a-4e60c8557a87
+begin
+	
+	clims = (-sᵤ * γ, sᵤ * γ)
+	
+	pess = heatmap(
+		supplyspace, pricespace, 
+		(s, p) -> r(s, p, 0.9), clim = clims,
+		title = "r(s, p; ψ = 0.9)",
+		xlabel = "current supply", ylabel = "price")
+	
+	opt = heatmap(
+		supplyspace, pricespace, 
+		(s, p) -> r(s, p, 1.1), clim = clims,
+		title = "r(s, p; ψ = 1.1)",
+		xlabel = "current supply", ylabel = "price")
+	
+	rfigure = plot(
+		pess, opt, 
+		size = ((800 + 150)*2*upscale, 600*upscale),
+		margin = 5Plots.mm
+	)
+end
+
+# ╔═╡ 52146730-b737-4b19-be11-2df335127b41
+savefig(rfigure, joinpath(plotpath, "rfunction.pdf"))
+
 # ╔═╡ ddf53627-181f-4fc0-b8dc-39d6243f1f0e
 md"""
 ## A small simulation with oscillating prices
 """
 
-# ╔═╡ 2004442b-6b1a-408a-a154-05eeba330b48
-function maker(c, ∂c∂s, ∂c∂r)
-	function r(p, s, ψ)
-		mc(s, r) = ∂c∂r(s, r)*r + c(s, r)
-		mb(s, r) = ψ*(p - k) + (∂c∂r(s+r, r) - ∂c∂s(s+r, r))*r + c(s+r, r)
-
-		root = fzero(r -> mc(s, r) - β*mb(s, r), 0.0)
-		
-		return max(-s, root)
-	end
-end
-
-# ╔═╡ 7eecc02c-9bc2-492c-8013-864bb99b029a
-r = maker(c, ∂c∂s, ∂c∂r)
-
 # ╔═╡ 27262d0f-06f5-4032-9a72-afd2e4cb4798
-function simulatecosprices(T, ψ; p₀ = 5., s₀ = 10.)
-	time = 1:T
-	supplies = ones(T) * s₀
-	ramps = zeros(T)
-	prices = ones(T) * p₀
+function simulatecosprices(T; p₀ = 5., s₀ = 10.)
 	
+	time = 1:T
+	
+	prices = ones(T) * p₀
 	for t=1:(T-1)
-		p = prices[t] + sin(t / 4)
-		s = supplies[t]
-		ramps[t+1] = r(p, s, ψ)
-		supplies[t+1] = s + ramps[t+1]
-		prices[t+1] = p
+		p = prices[t] + sin(t/10) * exp(- 3t / T)
+	end
+	
+	supplies = ones(T, 2) * s₀
+	ramps = zeros(T, 2)
+	
+	for (j, ψ) in enumerate([0.9, 1.1])
+			
+
+		for t=1:(T-1)
+			p = prices[t] + sin(t/2) * exp(- 3t / T)
+			s = supplies[t, j]
+
+			ramps[t+1, j] = r(s, p, ψ)
+
+			supplies[t+1, j] = max(s + ramps[t+1], 0.0)
+			prices[t+1] = p
+		end
+		
 	end
 	
 	return supplies, prices, ramps
@@ -161,34 +222,45 @@ end
 # ╔═╡ 0c2afa65-010c-4a06-aedb-112932a01c6d
 begin
 	T = 100
-	supplies, prices, ramps = simulatecosprices(T, 1.1)
+	supplies, prices, ramps = simulatecosprices(T; p₀ = 15)
+	"Done simulation with T = $T"
 end
 
 # ╔═╡ 3fdd4dc4-caf7-41f2-a6fc-a8dc89fa6fbc
 begin
-	simfig = plot(figsize = (1200, 800), dpi = 500)
 	
-	plot!(
-		simfig, 1:T, 
-		prices, label = "p", c=:red, legend = :topleft)
+	rstring = latexstring("\$ r(p_t, s_t, \\psi_t) \$")
 	
+	simfig = plot(
+		xlabel = "t",
+		title = rstring
+	)
+	
+	
+	for j in [1, 2]
+		plot!(
+			simfig, 2:T, linewidth = 3,
+			ramps[2:end, j], label = "r, ψ = $([0.9, 1.1][j])", ylabel = "r")	
+	end
 	
 	plot!(
 		twinx(simfig), 1:T, 
-		supplies, label = "s", c = :blue, legend = :topright)
+		prices, label = "p", alpha = 0.5,
+		c=:green, legend = :bottomleft)
+	
+	simfig
+	
 end
-
-# ╔═╡ 0802967f-811d-435b-832d-e4b60599f1b2
-plot(ramps[2:end], figsize = (1200, 800), dpi = 500, label = "r")
 
 # ╔═╡ Cell order:
 # ╠═d24cee46-d3f2-11eb-0f8c-93119645edd9
 # ╠═4509a0c9-374e-4f0f-a5dd-5671f0c67bb4
+# ╠═e2ac63a4-6440-41d6-9e05-6e31c6f366cb
 # ╠═805018be-7695-45ae-a671-2f8d8cc4a858
 # ╠═4412cc25-bb76-4453-9727-5cf097a883e1
 # ╟─e572d85f-1839-48e5-b1e2-2ee1123ef22e
 # ╟─48506150-b30a-4e05-ac66-886c88171783
-# ╟─5fdea5b6-10b6-4818-92e2-0d92d8bd0bcc
+# ╠═5fdea5b6-10b6-4818-92e2-0d92d8bd0bcc
 # ╟─ff79e521-c7a5-4ad2-96a0-0dc1c2c1c76c
 # ╠═f859798a-8229-4c54-90ed-fc8fdc833cec
 # ╠═4fda69af-82b4-4426-bc9d-aa8e9db4ec67
@@ -197,10 +269,11 @@ plot(ramps[2:end], figsize = (1200, 800), dpi = 500, label = "r")
 # ╠═b794fc16-e815-42d3-b767-d0630573383a
 # ╠═dfbdac59-6675-41a3-ab71-ac7f51f72a8a
 # ╠═93e56234-86d8-423b-8325-42d0ab2fa3de
+# ╠═b76606b0-3e1f-4d7d-bb88-b39e41827a67
+# ╠═52bc7382-42c8-4b4c-b188-3c8c9af281bf
+# ╟─9dbece0b-2aaf-475e-b45a-4e60c8557a87
+# ╠═52146730-b737-4b19-be11-2df335127b41
 # ╟─ddf53627-181f-4fc0-b8dc-39d6243f1f0e
-# ╠═2004442b-6b1a-408a-a154-05eeba330b48
-# ╠═7eecc02c-9bc2-492c-8013-864bb99b029a
-# ╠═27262d0f-06f5-4032-9a72-afd2e4cb4798
+# ╟─27262d0f-06f5-4032-9a72-afd2e4cb4798
 # ╠═0c2afa65-010c-4a06-aedb-112932a01c6d
-# ╠═3fdd4dc4-caf7-41f2-a6fc-a8dc89fa6fbc
-# ╠═0802967f-811d-435b-832d-e4b60599f1b2
+# ╟─3fdd4dc4-caf7-41f2-a6fc-a8dc89fa6fbc
---
+++ notebooks/provider.jl
@@ -0,0 +1,175 @@
+### A Pluto.jl notebook ###
+# v0.14.8
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 9270222c-4fa5-4332-b3e0-bf67f860eba5
+using Plots, LaTeXStrings
+
+# ╔═╡ cdb46928-3164-42c5-b391-8c8a43146347
+using LightGraphs, GraphPlot
+
+# ╔═╡ 3ce1d846-830c-41f5-9fc5-d4fc57627331
+using LinearAlgebra
+
+# ╔═╡ b496deda-d96b-11eb-0500-23f0345b125a
+begin
+	upscale = 1 #8x upscaling in resolution
+	fntsm = Plots.font("sans-serif", pointsize=round(10.0*upscale))
+	fntlg = Plots.font("sans-serif", pointsize=round(14.0*upscale))
+	default(titlefont=fntlg, guidefont=fntlg, tickfont=fntsm, legendfont=fntsm)
+	default(size=(800*upscale,600*upscale)) #Plot canvas size
+	
+	Plots.reset_defaults()
+	Plots.resetfontsizes()
+	
+	plotpath = "../plots/energy"
+	
+	"Plot path: $plotpath"
+end
+
+# ╔═╡ ed6671ce-5e62-46c9-a4f2-f91addcd9350
+"""
+Find a vector of n quantities xᵢ given their sum ∑xᵢ and a vector of n ratios with respect to one of them xᵢ / xₙ (note that xₙ / xₙ = 1 so one entry of the ratios ought to be 1)
+"""
+function xs_by_sum_ratio(sumX::Float64, ratios::Vector{Float64})
+	n = length(ratios)
+	xₙ = sumX / sum(ratios)
+	
+	return xₙ .* ratios
+	
+end
+
+# ╔═╡ 1ed8e95b-32e7-4638-ad49-0579fa87d56d
+function edgetotuple(edge)
+	(edge.src, edge.dst)
+end
+
+# ╔═╡ 335433a8-9cf4-4a34-ae33-5cdcd2261d0e
+begin
+	a, b = -5.0, 1.0
+	β = 0.8
+	
+	function X(p)
+		a + b * p
+	end
+	
+	function δ(i, j)
+		i < j ? 1 : -1
+	end
+	
+	function sortededge(i, j)
+		i < j ? (i, j) : (j, i)
+	end
+end
+
+# ╔═╡ a33a9c31-1f9d-4b3b-aa65-b398f92d5739
+begin
+	g = SimpleGraph(4)
+	
+	for j in 2:4
+		add_edge!(g, 1, j)
+	end
+	
+	# add_edge!(g, 2, 3)
+	
+	G = [
+		0 1 1;
+		1 0 1;
+		1 1 0
+	]
+end
+
+# ╔═╡ 2160fdfe-9196-4de8-8637-58aa87be550c
+begin
+	M = length(vertices(g))
+	∑p = - M * a / b
+	pratios = [rand() + 0.5 for m in 1:(M-1)]
+	push!(pratios, 1.)
+	
+	p = xs_by_sum_ratio(∑p, pratios)
+end
+
+# ╔═╡ fdbc67c6-0653-496c-934c-d58ce70be66b
+function getbargsolution(p::Vector{Float64}, g::SimpleGraph, G::Matrix{Int64})
+		
+	E = map(edgetotuple, edges(g))
+
+	Xs = map(X, p)
+	Δ = [Xs[i] - Xs[j] for (i, j) in E]
+	PY = 0.5*inv(2I + G)*Δ
+
+	PYmap = Dict(E .=> PY)
+	
+	Y = Dict(e => 0.0 for e in E)
+
+	for i in vertices(g)
+		
+		basej = last(neighbors(g, i))
+		baseedge = sortededge(i, basej)
+		
+		baseY = PYmap[baseedge] * δ(i, basej)
+				
+		Ei = [sortededge(i, j) for j in neighbors(g, i)]
+		
+		ratiosY = [PYmap[e] * δ(i, e[2]) for e in Ei]
+		
+		∑Y = Xs[i]
+		
+		Yi = xs_by_sum_ratio(∑Y, ratiosY)
+		Ymap = Dict(Ei .=> Yi)
+		
+		for e in Ei
+			if Y[e] == 0.0
+				Y[e] = Ymap[e]
+			end
+		end		
+	end
+	
+	P = Dict(e => PYmap[e] / Y[e] for e in E)
+	
+	return Y, P
+end
+
+# ╔═╡ 8f97a8af-0da0-4ac3-a0c4-300e9e0b327c
+Y, P = getbargsolution(p, g, G)
+
+# ╔═╡ b65639e2-29ef-4382-ae74-5a52e4bbfb5f
+function p′(p, g, G)
+	Y, P = getbargsolution(p, g, G)
+	
+	Xs = map(X, p)
+	
+	oneneigh = [sortededge(i, first(neighbors(g, i))) for i in 1:length(p)]
+	λ = [-2*P[i] for i in oneneigh]
+	
+	return  @. max(p + (a / b) + Xs * (1 - β) / (b * β) - λ, 0.0)
+end
+
+# ╔═╡ 2cf2565b-59bd-4a0c-8fb1-128b8f59fde2
+begin
+	newp =  p′(p, g, G)
+	newX = map(X, newp)
+	
+	newp, newX
+end
+
+# ╔═╡ caa7bfc5-242a-49c2-80f9-5491e8c8d253
+p
+
+# ╔═╡ Cell order:
+# ╠═9270222c-4fa5-4332-b3e0-bf67f860eba5
+# ╠═cdb46928-3164-42c5-b391-8c8a43146347
+# ╠═3ce1d846-830c-41f5-9fc5-d4fc57627331
+# ╟─b496deda-d96b-11eb-0500-23f0345b125a
+# ╠═ed6671ce-5e62-46c9-a4f2-f91addcd9350
+# ╠═1ed8e95b-32e7-4638-ad49-0579fa87d56d
+# ╠═335433a8-9cf4-4a34-ae33-5cdcd2261d0e
+# ╠═a33a9c31-1f9d-4b3b-aa65-b398f92d5739
+# ╠═2160fdfe-9196-4de8-8637-58aa87be550c
+# ╠═fdbc67c6-0653-496c-934c-d58ce70be66b
+# ╠═8f97a8af-0da0-4ac3-a0c4-300e9e0b327c
+# ╠═b65639e2-29ef-4382-ae74-5a52e4bbfb5f
+# ╠═2cf2565b-59bd-4a0c-8fb1-128b8f59fde2
+# ╠═caa7bfc5-242a-49c2-80f9-5491e8c8d253
