2ad47f02684b74c7c826a87cad7a79d052b0526d
NoFishLikeIan <andreatitton96@gmail.com>
2021-07-14 10:41:03 +0200

    🪐 examples code

--- notebooks/star.jl
+++ notebooks/star.jl
@@ -5,19 +5,45 @@ using Markdown
 using InteractiveUtils
 
 # ╔═╡ 41809916-cea7-11eb-2af5-8b37c6dbd767
-using Plots, DataFrames, CSV
-
-# ╔═╡ c0cf7f69-a656-4bc0-9c24-ad3dc0f31f36
-using Agents
-
-# ╔═╡ 848401cf-5016-4753-ba92-2e9e2350723b
-include("../src/main.jl")
+using Plots
 
 # ╔═╡ 40dc6db5-7b14-4150-89cc-ca27992a4b7c
-Plots.resetfontsizes(); Plots.scalefontsizes(0.8)
+using DataFrames, LinearAlgebra
 
-# ╔═╡ 3e067f0a-f882-421a-bdb0-bc4915aa5b35
-plotpath = "../plots/energy"
+# ╔═╡ 5e11eacb-f7b5-4914-b045-2684e4ad9779
+function ingredients(path::String)
+	# this is from the Julia source code (evalfile in base/loading.jl)
+	# but with the modification that it returns the module instead of the last object
+	name = Symbol(basename(path))
+	m = Module(name)
+	Core.eval(m,
+        Expr(:toplevel,
+             :(eval(x) = $(Expr(:core, :eval))($name, x)),
+             :(include(x) = $(Expr(:top, :include))($name, x)),
+             :(include(mapexpr::Function, x) = $(Expr(:top, :include))(mapexpr, $name, x)),
+             :(include($path))))
+	m
+end
+
+# ╔═╡ c0cf7f69-a656-4bc0-9c24-ad3dc0f31f36
+begin
+	Plots.resetfontsizes(); default()
+	default(size = (800, 600), margin = 10Plots.mm)
+	Plots.scalefontsizes(0.8)
+end
+
+# ╔═╡ 85620986-9b70-4c6b-baeb-dc47c715bee8
+begin
+	plotpath = "../plots/two"
+	simpath = "../simulation"
+end
+
+# ╔═╡ 848401cf-5016-4753-ba92-2e9e2350723b
+begin
+	main = ingredients(joinpath(simpath, "src/main.jl"))
+	simulate = ingredients(joinpath(simpath, "simulate.jl"))
+	barg = ingredients("bargaining.jl")
+end
 
 # ╔═╡ 658106ec-f84a-43c6-96ec-ec90e271a552
 begin
@@ -28,86 +54,18 @@ begin
 		1 0 0 0;
 	]
 
-	c₁ = 1.0
-
-	c(s, r) = log(1 + exp(c₁ * s * r))
-	∇c(s, r) = c₁ * inv(1 + exp(-c₁ * s * r))
-
-	∂c∂s(s, r) = ∇c(s, r) * r
-	∂c∂r(s, r) = ∇c(s, r) * s
-
-	ρₗ = ρᵤ = 0.9
-
-	ε = (
-		[
-			ρₗ 1 - ρₗ; 
-			1 - ρᵤ ρᵤ
-		],
-		[0.0, 10.0]
-	)
-
-	parameters = Dict(
-		:c => (c, ∂c∂s, ∂c∂r),
-		:Ψ => [0.9, 1.1],
-		:M => 500, :ε => ε,
-		:N => 15, :β => 0.99,
-		:βprod => 0.5
-	)
-
-	s₀ = 20.0
-
-	model = initializemodel(
-		A, parameters;
-		s₀=s₀, b₀=0.
-	)
+	G = makeG(A)
 end
 
-# ╔═╡ ad90abb5-2e74-49f2-ab24-76265904afb8
-function makeproducer(ψ)
-	s -> Producer(1, 1, s, 1.0, ψ, 1.0, [1., 1.])
-end
-
-# ╔═╡ c60d5bdf-4052-4bb2-9628-bc4f2be3579f
-function drawrampup(ψ, supplies)
-	prices = range(0., 20., length = 1000)
-	
-	producer = makeproducer(ψ)
-	
-	function safer(s, p)
-		try
-			return r(p, producer(s), model)
-		catch
-			println("Error with s=$s and p=$p")
-			
-			return 0.0
-		end
-
-	end
-		
-	figure = plot(xlabel = "p", ylabel="r")
-	
-	for s ∈ supplies
-		plot!(figure, p -> r(p, producer(s), model), label="s = $s")
-	end
-	
-	
-	return figure
-end
-
-# ╔═╡ b20e801b-515b-4142-b278-df4a46201659
-drawrampup(2.5, [2.])
-
-# ╔═╡ f9e927f1-dbf0-497a-a539-f4897687536d
-dfagent = CSV.read("../data/out/star_sim.csv", DataFrame)
+# ╔═╡ 0c6fdbe0-8af1-4139-8950-27da3b18b370
+barg.plotpower(A, G)
 
 # ╔═╡ Cell order:
 # ╠═41809916-cea7-11eb-2af5-8b37c6dbd767
 # ╠═40dc6db5-7b14-4150-89cc-ca27992a4b7c
+# ╟─5e11eacb-f7b5-4914-b045-2684e4ad9779
 # ╠═c0cf7f69-a656-4bc0-9c24-ad3dc0f31f36
+# ╠═85620986-9b70-4c6b-baeb-dc47c715bee8
 # ╠═848401cf-5016-4753-ba92-2e9e2350723b
-# ╠═3e067f0a-f882-421a-bdb0-bc4915aa5b35
-# ╟─658106ec-f84a-43c6-96ec-ec90e271a552
-# ╠═ad90abb5-2e74-49f2-ab24-76265904afb8
-# ╠═c60d5bdf-4052-4bb2-9628-bc4f2be3579f
-# ╠═b20e801b-515b-4142-b278-df4a46201659
-# ╟─f9e927f1-dbf0-497a-a539-f4897687536d
+# ╠═658106ec-f84a-43c6-96ec-ec90e271a552
+# ╠═0c6fdbe0-8af1-4139-8950-27da3b18b370
--- notebooks/two.jl
+++ notebooks/two.jl
@@ -8,13 +8,28 @@ using InteractiveUtils
 using Plots
 
 # ╔═╡ 0a384684-66ff-4c23-8ff5-3bf9074307f8
-using DataFrames
+using DataFrames, LinearAlgebra
 
 # ╔═╡ 9c1e1856-7ebf-4330-b4a4-d077cace9fb1
 begin
 	Plots.resetfontsizes(); default()
 	default(size = (800, 600), margin = 10Plots.mm)
 	Plots.scalefontsizes(0.8)
+end
+
+# ╔═╡ 5d095408-ab8f-4209-bd0a-97660b854696
+function ingredients(path::String)
+	# this is from the Julia source code (evalfile in base/loading.jl)
+	# but with the modification that it returns the module instead of the last object
+	name = Symbol(basename(path))
+	m = Module(name)
+	Core.eval(m,
+        Expr(:toplevel,
+             :(eval(x) = $(Expr(:core, :eval))($name, x)),
+             :(include(x) = $(Expr(:top, :include))($name, x)),
+             :(include(mapexpr::Function, x) = $(Expr(:top, :include))(mapexpr, $name, x)),
+             :(include($path))))
+	m
 end
 
 # ╔═╡ 4de328e3-21e1-4a98-b69b-3e05471b89e1
@@ -24,10 +39,10 @@ begin
 end
 
 # ╔═╡ 1781deee-e3af-11eb-0651-fbbc9df7a22b
-include(joinpath(simpath, "src/main.jl"))
-
-# ╔═╡ 71da0c79-de96-43c2-8c08-c8bc05fc5044
-include(joinpath(simpath, "simulate.jl"))
+begin
+	main = ingredients(joinpath(simpath, "src/main.jl"))
+	simulate = ingredients(joinpath(simpath, "simulate.jl"))
+end
 
 # ╔═╡ 8a855dc7-9301-4262-90f1-1d1749f9c83f
 md"""
@@ -48,7 +63,7 @@ end
 
 # ╔═╡ 64e04d91-9b0a-4afa-bad1-381a5fe4e0ee
 begin
-	T = 100
+	T = 250
 	lowε, highε = last(default_params[:ε])
 end
 
@@ -64,13 +79,16 @@ begin
 end
 
 # ╔═╡ 4806b941-f97d-407e-b3bf-cb1c983a45bf
-model = initializemodel(A, G, default_params; εpath = makeshock(10, 15))
+model = main.initializemodel(A, G, default_params; εpath = makeshock(50, 70))
 
 # ╔═╡ f6c51ca8-bf2d-428c-9872-2ab0a8ffe52c
 dfagent, dfmodel = simulatemarket!(model; T = T)
 
 # ╔═╡ c9580e63-7f3c-4630-80d3-04ef1c78cc19
-plotexcessdemand(dfagent, dfmodel, savepath = joinpath(plotpath, "demand.pdf"))
+begin
+	plotexcessdemand(dfagent, dfmodel, savepath = joinpath(plotpath, "demand.pdf"))
+	plotexcessdemand(dfagent, dfmodel)
+end
 
 # ╔═╡ c3ad6d28-3dfe-40c4-a77b-919368366953
 pricesupplyplot(dfagent, model; savepath = joinpath(plotpath, "pricesupply.pdf"))
@@ -100,13 +118,16 @@ begin
 	end
 end
 
+# ╔═╡ 43256d6c-e50f-4c0d-b66f-2237355c8a1f
+plotproviderbeliefs(dfagent, model)
+
 # ╔═╡ Cell order:
 # ╠═321f5b93-604c-4ad0-b41c-345cfbfa4f2f
 # ╠═0a384684-66ff-4c23-8ff5-3bf9074307f8
 # ╠═9c1e1856-7ebf-4330-b4a4-d077cace9fb1
+# ╟─5d095408-ab8f-4209-bd0a-97660b854696
 # ╠═4de328e3-21e1-4a98-b69b-3e05471b89e1
 # ╠═1781deee-e3af-11eb-0651-fbbc9df7a22b
-# ╠═71da0c79-de96-43c2-8c08-c8bc05fc5044
 # ╟─8a855dc7-9301-4262-90f1-1d1749f9c83f
 # ╠═d54ae3c4-62cf-4e79-9bfa-8ba7fac06d7a
 # ╠═64e04d91-9b0a-4afa-bad1-381a5fe4e0ee
@@ -116,3 +137,4 @@ end
 # ╠═c9580e63-7f3c-4630-80d3-04ef1c78cc19
 # ╠═c3ad6d28-3dfe-40c4-a77b-919368366953
 # ╠═c4230c07-433f-4654-8708-b600fecdcb61
+# ╠═43256d6c-e50f-4c0d-b66f-2237355c8a1f
